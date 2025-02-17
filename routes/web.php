cda395daa21b28303f461ad48e3ac3355140982f
DcSebas95 <sebas13_diaz@hotmail.com>
2025-02-16 21:52:18 -0500

    enviogit

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,26 @@
 <?php
 
 use App\Http\Controllers\CocktailController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Rutas protegidas (se requiere estar logueado)
+// Rutas protegidas (requiere estar logueado)
 Route::middleware(['auth'])->group(function () {
     Route::get('/cocktails', [CocktailController::class, 'index'])->name('cocktails.index');
     Route::post('/cocktails/save', [CocktailController::class, 'save'])->name('cocktails.save');
     Route::get('/my-cocktails', [CocktailController::class, 'myCocktails'])->name('cocktails.my');
-    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-    Route::middleware(['auth'])->group(function () {
-    Route::get('/cocktails/{id}/edit', [App\Http\Controllers\CocktailController::class, 'edit'])->name('cocktails.edit');
-    Route::put('/cocktails/{id}', [App\Http\Controllers\CocktailController::class, 'update'])->name('cocktails.update');
-    Route::delete('/cocktails/{id}', [App\Http\Controllers\CocktailController::class, 'destroy'])->name('cocktails.destroy');
-    });
-    
-
-
-    
-    // Rutas para editar y eliminar (se pueden agregar aquí)
+    Route::get('/cocktails/{id}/edit', [CocktailController::class, 'edit'])->name('cocktails.edit');
+    Route::put('/cocktails/{id}', [CocktailController::class, 'update'])->name('cocktails.update');
+    Route::delete('/cocktails/{id}', [CocktailController::class, 'destroy'])->name('cocktails.destroy');
 });
 
+// Autenticación
 Auth::routes();
 
-
+// Ruta principal para la página de inicio
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('cocktails', CocktailController::class)->middleware('auth');
+
